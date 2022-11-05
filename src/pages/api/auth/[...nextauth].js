6f2695a59e9bbbc57177eac34d0ce2b8df0989a1
haaleel <haleeludheen@gmail.com>
2022-11-05 21:01:43 +0530

    update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,32 +1,17 @@
-// import NextAuth from "next-auth"
+import NextAuth from "next-auth"
 
-// import GoogleProvider from "next-auth/providers/google";
-// export const  authOptions = ({
-//   // Configure one or more authentication providers
-//   providers: [
-//    GoogleProvider({
-//       clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_SECRET,
-//     }),
-//     // ...add more providers here 
-//     // providers: [
-  
-//   ],
-//   // secret: process.env.NEXTAUTH_SECRET,
-// })
-// export default NextAuth(authOptions)    
-
-
-
-import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
-export default NextAuth({
-   providers: [
-     GoogleProvider({
+export const  authOptions = ({
+  // Configure one or more authentication providers
+  providers: [
+   GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-     }),
-   ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
+    }),
+    // ...add more providers here 
+    // providers: [
+  
+  ],
+  secret: process.env.JWT_SECRET,
+})
+export default NextAuth(authOptions)    
